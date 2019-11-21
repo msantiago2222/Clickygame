10acb135a06745f201a75785ad3134f8c2f4c4bd
Michelle Santiago <msantiago2222@yahoo.com>
2019-11-20 19:32:08 -0800

    finished scoring

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ClickyGame from './components/ClickyGame';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ClickyGame />, document.getElementById('root'));
