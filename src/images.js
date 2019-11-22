c0c504777845a5d0d022f84d82eac3e2ab20d729
Michelle Santiago <msantiago2222@yahoo.com>
2019-11-21 21:02:10 -0800

    images added, connected.

---
+++ src/images.js
@@ -0,0 +1,29 @@
+import cat from './images/cat.png';
+import catone from './images/catone.png';
+import cattwo from './images/cattwo.png';
+import catthree from './images/catthree.png';
+import catfour from './images/catfour.png';
+import catfive from './images/catfive.png';
+import catsix from './images/catsix.png';
+import catseven from './images/catseven.png';
+import cateight from './images/cateight.png';
+import catnine from './images/catnine.png';
+import cateleven from './images/cateleven.png';
+import cattweleve from './images/cattweleve.png';
+
+const images = [
+  cat,
+  catone,
+  cattwo,
+  catthree,
+  catfour,
+  catfive,
+  catsix,
+  catseven,
+  cateight,
+  catnine,
+  cateleven,
+  cattweleve
+];
+
+export default images;
