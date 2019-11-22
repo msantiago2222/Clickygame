e5b58fc4dc6cf3692acba9e5cc4b83114f016b58
Michelle Santiago <msantiago2222@yahoo.com>
2019-11-21 20:22:30 -0800

    save

---
+++ build/precache-manifest.a9d94361f6e44650e2fe3c51152fc0ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21fbf39af6172923644a9aaf6a606380",
+    "url": "/Clickygame/index.html"
+  },
+  {
+    "revision": "66c74fb946aeee5b663c",
+    "url": "/Clickygame/static/css/main.2cad1bdb.chunk.css"
+  },
+  {
+    "revision": "c10930011019739353e7",
+    "url": "/Clickygame/static/js/2.c48ebf3b.chunk.js"
+  },
+  {
+    "revision": "66c74fb946aeee5b663c",
+    "url": "/Clickygame/static/js/main.f4445add.chunk.js"
+  },
+  {
+    "revision": "020304da66b7b05d6d53",
+    "url": "/Clickygame/static/js/runtime-main.ef4471ac.js"
+  }
+]);
